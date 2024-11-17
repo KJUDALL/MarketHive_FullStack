e4bcbfab2f2318f36a8f56d31e80506be3c215c7
Kjorgy <164044713+KJUDALL@users.noreply.github.com>
2024-11-17 16:42:44 -0500

    updated folder structure, specifically for the data models I'm trying to build out. Also installed all the necessary npm packages. Added PR's to our github repo for merging purposes to maintain the protection of the main branch as we collaborate.

--- client/src/Main.jsx
+++ client/src/Main.jsx
@@ -1,2 +1,2 @@
-// Main entry point of our React app - renders the App componenet into the DOM
+// Main entry point of our React app - renders the App.jsx componenet using ReactDOM
 // Include setup for React and any global configs
--- server/src/db/schema.sql
+++
@@ -1,1 +0,0 @@
--- Defines stucture of DB tables
