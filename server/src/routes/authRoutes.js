806b654fa5096837fd45090d91ba80659fce8a84
Kjorgy <164044713+KJUDALL@users.noreply.github.com>
2024-11-18 20:53:53 -0500

    updated folder structure and vite install to help with the Render build/deployment. Additional backend server files updates as well.

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,1 +1,17 @@
 // Routes related to authentication
+//Handles authentication routes like /login or /signup
+import express from 'express';
+import { registration, loginUser, logoutUser } from '../controllers/user-controller';
+import router from 'express.router';
+import { defineConfig } from 'vite';
+
+//register new user
+router.post('/registration', registration);
+
+//login user
+router.post('/login', loginUser);
+
+//logout user
+router.post('/logout', logoutUser);
+
+export default router;
