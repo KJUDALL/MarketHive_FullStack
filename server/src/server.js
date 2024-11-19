4f115390a87142912776ace18830ed6d3f65a8b8
Kjorgy <164044713+KJUDALL@users.noreply.github.com>
2024-11-18 21:22:04 -0500

    updates for the server in server.js file

--- server/src/server.js
+++ server/src/server.js
@@ -4,6 +4,11 @@ const forceDatabaseRefresh = false;
 import express from 'express';
 import { Sequelize } from 'sequelize';
 
+const app = express() 
+
 const PORT = process.env.PORT || 3001;
 
 //Main entry point for the back end. Ensure it imports and uses routes, connects to the database, and configures middleware (e.g., express.json()).
+app.listen(PORT, () => {
+    console.log('Server has started.')
+});
